a9b96bc74342eabb09607dd2eabce2feaa30aa23
zhurof <41473707+zhurof@users.noreply.github.com>
2019-09-14 19:30:41 +0300

    edits

--- js/script.js
+++ js/script.js
@@ -1,1 +1,22 @@
-﻿
+﻿$(window).on('load scroll',function(){
+	if(this.scrollY){
+		$('.header').addClass('fixed');
+	}else{
+		$('.header').removeClass('fixed');
+	}
+})
+$('.header__menu a[href^="#"]').click(function(e){
+	e.preventDefault();
+	if($(this.hash).length){		
+		var top = $(this.hash).offset().top - $('.header').outerHeight();
+		$('html').animate({
+			scrollTop: top
+		},600);
+	}
+	$('.header__menu').removeClass('open');
+	$('.menu-btn').removeClass('active');
+})
+$('.menu-btn').click(function(){
+	$(this).toggleClass('active');
+	$('.header__menu').toggleClass('open');
+})
